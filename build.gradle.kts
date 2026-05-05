1ba7f96d4913fd0289ec3bd3b6176687f8d40209
Orchiwi <orchiwideveloper@outlook.com>
2026-05-05 22:21:15 +0200

    build: add MaxMind GeoIP2 and Caffeine deps with Shadow relocation
    Adds:
    - com.maxmind.geoip2:geoip2:4.2.1 (MMDB country/anonymous-IP lookups)
    - com.github.ben-manes.caffeine:caffeine:3.1.8 (lookup cache)
    - com.gradleup.shadow:8.3.6 (fat jar with relocations)
    
    Relocates third-party packages under fr.horizonsmp.geoBlock.lib to avoid
    classpath collisions with other plugins on the server.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     alias(libs.plugins.run.paper)
+    alias(libs.plugins.shadow)
 }
 
 repositories {
@@ -10,6 +11,9 @@ repositories {
 
 dependencies {
     compileOnly(libs.paper.api)
+
+    implementation(libs.geoip2)
+    implementation(libs.caffeine)
 }
 
 java {
@@ -20,12 +24,32 @@ tasks {
     runServer {
         minecraftVersion(libs.versions.minecraft.get())
         jvmArgs("-Xms2G", "-Xmx2G")
+        pluginJars(shadowJar.flatMap { it.archiveFile })
     }
 
     processResources {
-        val props = mapOf("version" to version )
+        val props = mapOf("version" to version)
         filesMatching("plugin.yml") {
             expand(props)
         }
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+        val shadedBase = "fr.horizonsmp.geoBlock.lib"
+        relocate("com.maxmind", "$shadedBase.maxmind")
+        relocate("com.github.benmanes.caffeine", "$shadedBase.caffeine")
+        relocate("com.fasterxml.jackson", "$shadedBase.jackson")
+        relocate("org.checkerframework", "$shadedBase.checkerframework")
+        relocate("com.google.errorprone", "$shadedBase.errorprone")
+        mergeServiceFiles()
+    }
+
+    jar {
+        archiveClassifier.set("plain")
+    }
+
+    build {
+        dependsOn(shadowJar)
     }
 }

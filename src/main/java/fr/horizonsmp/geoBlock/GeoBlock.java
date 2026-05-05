72a86e8b5f4bba2856da019f5aa9c558c9830ea5
Orchiwi <orchiwideveloper@outlook.com>
2026-05-05 22:27:32 +0200

    feat: load configuration and messages on plugin enable
    Wires ConfigLoader and Messages into the plugin lifecycle, exposes
    accessors for downstream services and provides a single reloadAll()
    entry point that will be reused by the /geoblock reload command.

--- src/main/java/fr/horizonsmp/geoBlock/GeoBlock.java
+++ src/main/java/fr/horizonsmp/geoBlock/GeoBlock.java
@@ -1,16 +1,40 @@
 package fr.horizonsmp.geoBlock;
 
+import fr.horizonsmp.geoBlock.config.ConfigLoader;
+import fr.horizonsmp.geoBlock.config.PluginConfig;
+import fr.horizonsmp.geoBlock.i18n.Messages;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GeoBlock extends JavaPlugin {
 
+    private ConfigLoader configLoader;
+    private Messages messages;
+    private PluginConfig config;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.configLoader = new ConfigLoader(this);
+        this.messages = new Messages(this);
+        reloadAll();
+        getLogger().info("GeoBlock loaded in " + config.mode() + " mode with "
+                + config.countries().size() + " countries.");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void reloadAll() {
+        this.config = configLoader.load();
+        this.messages.load();
+    }
+
+    public PluginConfig pluginConfig() {
+        return config;
+    }
+
+    public Messages messages() {
+        return messages;
     }
 }

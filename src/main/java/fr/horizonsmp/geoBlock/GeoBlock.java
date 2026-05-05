fa03b009a36356cc7e2108d527cf359770e09314
Orchiwi <orchiwideveloper@outlook.com>
2026-05-05 22:20:05 +0200

    chore: initial project skeleton

---
+++ src/main/java/fr/horizonsmp/geoBlock/GeoBlock.java
@@ -0,0 +1,16 @@
+package fr.horizonsmp.geoBlock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GeoBlock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
